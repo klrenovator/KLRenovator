408c87a8891d965912d95ac0b554ee8c11df763b
klrenovator <info@klrenovator.com>
2026-06-03 12:49:37 +0800

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import * as React from "react";
+import { LanguageProvider } from "@/context/language-context";
 
 export interface ProvidersProps {
   children: React.ReactNode;
 }
 
 export function Providers({ children }: ProvidersProps) {
-  return <>{children}</>;
+  return <LanguageProvider>{children}</LanguageProvider>;
 }
