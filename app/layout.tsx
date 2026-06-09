b451ad43d7824a4aa940386b516e8c182f93c0a0
klrenovator <info@klrenovator.com>
2026-06-10 03:03:48 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -445,6 +445,21 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
+        {/* Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "x4h3dghn6p");
+            `,
+          }}
+        />
+
         {/* Google Analytics GA4 */}
         <Script
           strategy="afterInteractive"
