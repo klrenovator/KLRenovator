eff4890de9c4f0bc3d288355e6854495b331336b
klrenovator <info@klrenovator.com>
2026-06-24 08:46:08 +0800

    Add files via upload

--- components/service-icon.tsx
+++ components/service-icon.tsx
@@ -1,4 +1,4 @@
-import { FaSnowflake, FaTools, FaWrench } from "react-icons/fa";
+import { FaSnowflake, FaTools, FaWrench, FaTriangleExclamation } from "react-icons/fa6";
 import { GiChemicalDrop, GiGasPump } from "react-icons/gi";
 import { LuLayoutGrid, LuMove, LuPlug, LuSettings } from "react-icons/lu";
 import type { IconType } from "react-icons";
@@ -12,6 +12,7 @@ const map: Record<string, IconType> = {
   "layout-grid": LuLayoutGrid,
   move: LuMove,
   plug: LuPlug,
+  "alert-triangle": FaTriangleExclamation,
 };
 
 export const ServiceIcon = ({
