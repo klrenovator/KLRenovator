b0fc99ff2ad370b19367c9d2291b65dc57acb697
klrenovator <info@klrenovator.com>
2026-07-07 01:50:26 +0800

    Add files via upload

--- app/problems/page.tsx
+++ app/problems/page.tsx
@@ -74,6 +74,7 @@ export default function ProblemsPage() {
             sizes="100vw"
             className="object-cover"
             loading="lazy"
+            decoding="async"
           />
         </div>
         <div className="absolute inset-0 bg-gradient-to-r from-white/95 via-white/85 to-white/60" />
