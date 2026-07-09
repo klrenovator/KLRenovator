07d9f9a501c02e37a34fd1a0a44e6ce5a28d7810
klrenovator <info@klrenovator.com>
2026-07-09 14:28:54 +0800

    Add files via upload

--- app/zh/areas/[slug]/[kampung]/page.tsx
+++ app/zh/areas/[slug]/[kampung]/page.tsx
@@ -5,6 +5,7 @@ import { FaWhatsapp } from "react-icons/fa6";
 import { FiArrowRight, FiChevronRight, FiMapPin } from "react-icons/fi";
 
 import { siteConfig } from "@/config/site";
+import { clampMetaTitle } from "@/lib/seo-title-optimizer";
 import { allPosts } from "@/config/blog-posts";
 import { Reveal } from "@/components/reveal";
 import { title } from "@/components/primitives";
@@ -39,10 +40,10 @@ export async function generateMetadata({
   const zhUrl = `https://www.klrenovator.com/zh/areas/${slug}/${kampung}`;
 
   return {
-    title: k.metaTitleZH || k.metaTitle,
+    title: clampMetaTitle(k.metaTitleZH || k.metaTitle),
     description: k.metaDescZH || k.metaDesc,
     openGraph: {
-      title: k.metaTitleZH || k.metaTitle,
+      title: clampMetaTitle(k.metaTitleZH || k.metaTitle),
       description: k.metaDescZH || k.metaDesc,
       url: zhUrl,
       type: "website",
