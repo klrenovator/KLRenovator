7f6e7d8211ff56200a8b8e61dfd38595f5ebc6ed
klrenovator <info@klrenovator.com>
2026-07-09 14:43:39 +0800

    Add files via upload

--- app/zh/problems/[slug]/page.tsx
+++ app/zh/problems/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { FaWhatsapp } from "react-icons/fa6";
 import { FiArrowRight, FiChevronRight, FiAlertTriangle, FiTool } from "react-icons/fi";
 
 import { siteConfig } from "@/config/site";
+import { clampMetaTitle } from "@/lib/seo-title-optimizer";
 import { problemContent } from "@/app/problems/[slug]/page";
 import { problemAEOContent } from "@/config/problem-aeo-content";
 import { PROBLEM_BRAND_MAP } from "@/config/topical-authority-map";
@@ -35,10 +36,10 @@ export async function generateMetadata({
   const zhUrl = `https://www.klrenovator.com/zh/problems/${slug}`;
 
   return {
-    title: problem.metaTitleZH || problem.metaTitle,
+    title: clampMetaTitle(problem.metaTitleZH || problem.metaTitle),
     description: problem.metaDescZH || problem.metaDesc,
     openGraph: {
-      title: problem.metaTitleZH || problem.metaTitle,
+      title: clampMetaTitle(problem.metaTitleZH || problem.metaTitle),
       description: problem.metaDescZH || problem.metaDesc,
       url: zhUrl,
       type: "website",
