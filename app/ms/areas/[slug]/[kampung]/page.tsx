fc0dd2cb8285641f78603bfae297700df2358a73
klrenovator <info@klrenovator.com>
2026-07-09 15:58:29 +0800

    Add files via upload

--- app/ms/areas/[slug]/[kampung]/page.tsx
+++ app/ms/areas/[slug]/[kampung]/page.tsx
@@ -6,6 +6,7 @@ import { FiArrowRight, FiChevronRight, FiMapPin } from "react-icons/fi";
 
 import { siteConfig } from "@/config/site";
 import { clampMetaTitle } from "@/lib/seo-title-optimizer";
+import { clampMetaDescription } from "@/lib/seo-description-optimizer";
 import { allPosts } from "@/config/blog-posts";
 import { Reveal } from "@/components/reveal";
 import { title } from "@/components/primitives";
@@ -42,10 +43,10 @@ export async function generateMetadata({
 
   return {
     title: clampMetaTitle(k.metaTitleMS || k.metaTitle),
-    description: k.metaDescMS || k.metaDesc,
+    description: clampMetaDescription(k.metaDescMS || k.metaDesc),
     openGraph: {
       title: clampMetaTitle(k.metaTitleMS || k.metaTitle),
-      description: k.metaDescMS || k.metaDesc,
+      description: clampMetaDescription(k.metaDescMS || k.metaDesc),
       url: msUrl,
       type: "website",
       locale: "ms_MY",
