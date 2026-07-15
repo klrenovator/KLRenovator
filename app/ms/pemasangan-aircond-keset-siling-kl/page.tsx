c34faa50fe908bcdbf67231e37f903ec2743edc3
klrenovator <info@klrenovator.com>
2026-07-15 14:31:49 +0800

    Add files via upload

--- app/ms/pemasangan-aircond-keset-siling-kl/page.tsx
+++ app/ms/pemasangan-aircond-keset-siling-kl/page.tsx
@@ -1,1 +1,8 @@
+import { InstallationLandingPage } from "@/components/installation-landing-page";
+import { getInstallationMetadata } from "@/config/installation-page-content";
 
+export const metadata = getInstallationMetadata("ceiling-cassette", "ms");
+
+export default function Page() {
+  return <InstallationLandingPage pageKey="ceiling-cassette" locale="ms" />;
+}
