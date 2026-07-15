f722022f96e8865f4c3c4221a2f078c17c82fe66
klrenovator <info@klrenovator.com>
2026-07-15 14:42:33 +0800

    Add files via upload

--- app/window-unit-aircond-installation-kl/page.tsx
+++ app/window-unit-aircond-installation-kl/page.tsx
@@ -1,1 +1,8 @@
+import { InstallationLandingPage } from "@/components/installation-landing-page";
+import { getInstallationMetadata } from "@/config/installation-page-content";
 
+export const metadata = getInstallationMetadata("window-unit", "en");
+
+export default function Page() {
+  return <InstallationLandingPage pageKey="window-unit" locale="en" />;
+}
