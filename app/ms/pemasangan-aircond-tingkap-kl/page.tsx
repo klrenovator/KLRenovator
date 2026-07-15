d271eb798de2dd992cad21e08e8d5d557a87ea28
klrenovator <info@klrenovator.com>
2026-07-15 14:43:36 +0800

    Add files via upload

--- app/ms/pemasangan-aircond-tingkap-kl/page.tsx
+++ app/ms/pemasangan-aircond-tingkap-kl/page.tsx
@@ -1,1 +1,8 @@
+import { InstallationLandingPage } from "@/components/installation-landing-page";
+import { getInstallationMetadata } from "@/config/installation-page-content";
 
+export const metadata = getInstallationMetadata("window-unit", "ms");
+
+export default function Page() {
+  return <InstallationLandingPage pageKey="window-unit" locale="ms" />;
+}
