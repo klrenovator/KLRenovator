230095287b43f58cdf3f2b0b4c99e92e31abf57b
klrenovator <info@klrenovator.com>
2026-07-15 14:25:13 +0800

    Add files via upload

--- app/ceiling-cassette-aircond-installation-kl/page.tsx
+++ app/ceiling-cassette-aircond-installation-kl/page.tsx
@@ -1,1 +1,8 @@
+import { InstallationLandingPage } from "@/components/installation-landing-page";
+import { getInstallationMetadata } from "@/config/installation-page-content";
 
+export const metadata = getInstallationMetadata("ceiling-cassette", "en");
+
+export default function Page() {
+  return <InstallationLandingPage pageKey="ceiling-cassette" locale="en" />;
+}
