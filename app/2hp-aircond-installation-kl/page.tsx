63ad864c53de5e9def2b8c122db08ba1acd80949
klrenovator <info@klrenovator.com>
2026-07-15 14:22:17 +0800

    Add files via upload

--- app/2hp-aircond-installation-kl/page.tsx
+++ app/2hp-aircond-installation-kl/page.tsx
@@ -1,1 +1,8 @@
+import { InstallationLandingPage } from "@/components/installation-landing-page";
+import { getInstallationMetadata } from "@/config/installation-page-content";
 
+export const metadata = getInstallationMetadata("2hp", "en");
+
+export default function Page() {
+  return <InstallationLandingPage pageKey="2hp" locale="en" />;
+}
