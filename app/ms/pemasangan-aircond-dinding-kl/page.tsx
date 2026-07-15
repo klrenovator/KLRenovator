da8ff6a5cbf5a779cb8cc73a369b20accca5a85d
klrenovator <info@klrenovator.com>
2026-07-15 14:30:20 +0800

    Add files via upload

--- app/ms/pemasangan-aircond-dinding-kl/page.tsx
+++ app/ms/pemasangan-aircond-dinding-kl/page.tsx
@@ -1,1 +1,8 @@
+import { InstallationLandingPage } from "@/components/installation-landing-page";
+import { getInstallationMetadata } from "@/config/installation-page-content";
 
+export const metadata = getInstallationMetadata("wall-mounted", "ms");
+
+export default function Page() {
+  return <InstallationLandingPage pageKey="wall-mounted" locale="ms" />;
+}
