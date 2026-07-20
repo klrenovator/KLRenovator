8884cd447eb441ed4a712ca1845ae6440dbc46ce
klrenovator <info@klrenovator.com>
2026-07-20 10:23:18 +0800

    Add files via upload

--- app/ms/areas/[slug]/page.tsx
+++ app/ms/areas/[slug]/page.tsx
@@ -624,7 +624,7 @@ export default async function AreaPageMS({
                   </NextLink>
                 ))}
                 <NextLink
-                  href="/areas"
+                  href="/ms/areas"
                   className="inline-flex items-center gap-1.5 border border-sky-200 bg-sky-50 px-3 py-1.5 text-xs font-black text-sky-700 hover:bg-sky-100 transition rounded-xl"
                 >
                   Semua Kawasan <FiArrowRight className="h-3 w-3" />
