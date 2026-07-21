c66241dbf9f623903db0859d0be48309db3f92f7
klrenovator <info@klrenovator.com>
2026-07-21 11:32:52 +0800

    Add files via upload

--- config/site.ts
+++ config/site.ts
@@ -8049,7 +8049,7 @@ export const siteConfig = {
 
   stats: [
     { label: "Happy Klang Valley Customers", value: "5,000+" },
-    { label: "Years HVAC Experience", value: "10+" },
+    { label: "Years HVAC Experience", value: "12+" },
     { label: "5-Star Verified Reviews", value: "500+" },
     { label: "Emergency Response Time", value: "< 30 min" },
   ],
